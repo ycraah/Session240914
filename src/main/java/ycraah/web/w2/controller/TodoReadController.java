629c9154537e9d368094a73e7d928b48e364c2c4
ycraah <ycraah@gmail.com>
2024-09-14 22:33:46 +0900

    쿠키를 통해 기ë열람 기록 확인

--- src/main/java/ycraah/web/w2/controller/TodoReadController.java
+++ src/main/java/ycraah/web/w2/controller/TodoReadController.java
@@ -6,6 +6,7 @@ import ycraah.web.w2.service.TodoService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,10 +22,54 @@ public class TodoReadController extends HttpServlet {
       Long tno = Long.parseLong(req.getParameter("tno"));
       TodoDTO todoDTO = TodoService.INSTANCE.get(tno);
       req.setAttribute("todoDTO", todoDTO);
+
+      Cookie viewTodoCookie = findCookie(req.getCookies(), "viewTodos");
+      String todoListStr = viewTodoCookie.getValue();
+      boolean exist = false;
+
+      //쿠키에 tno값이 저장되어 있는지 검색
+      if(todoListStr != null && todoListStr.indexOf(tno+"-") >= 0) {
+        exist = true;
+      }
+
+      log.info("exist: " + exist);
+
+      //검색 내용이 없으면 쿠키에 기록 저장
+      if(!exist){
+        todoListStr += tno+"-";
+        viewTodoCookie.setValue(todoListStr);
+        viewTodoCookie.setMaxAge(60*60*24);
+        viewTodoCookie.setPath("/");
+        resp.addCookie(viewTodoCookie);
+      }
+
       req.getRequestDispatcher("/todo/read.jsp").forward(req, resp);
     } catch (Exception e) {
+      e.printStackTrace();
       log.error(e.getMessage());
       throw new ServletException("TodoRead(GET) 오류");
     }
+  }
+
+  private Cookie findCookie(Cookie[] cookies, String cookieName) {
+    Cookie targetCookie = null;
+
+    //viewTodos 쿠키가 존재하는 지 확인
+    if(cookies != null && cookies.length > 0) {
+      for(Cookie ck : cookies){
+        if(ck.getName().equals(cookieName)){
+          targetCookie = ck;
+          break;
+        }
+      }
+    }
+    //존재하지 않으면 쿠키 생성
+    if(targetCookie == null) {
+      targetCookie = new Cookie(cookieName, "");
+      targetCookie.setPath("/");
+      targetCookie.setMaxAge(60*60*24);
+    }
+    //쿠키 반환
+    return targetCookie;
   }
 }
